93ea802b29fa3785ac03496a7b54f11c2fc41a94
Code2Prod <office@code2prog.com>
2020-10-14 16:48:58 +0700

    new entities

---
+++ tests/FedexRest/Tests/Entity/PersonEntityTest.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace FedexRest\Tests\Entity;
+
+
+use PHPUnit\Framework\TestCase;
+
+class PersonEntityTest extends TestCase
+{
+
+}
--- tests/FedexRest/Tests/Ship/CreateTagRequestTest.php
+++ tests/FedexRest/Tests/Ship/CreateTagRequestTest.php
@@ -28,18 +28,18 @@ class CreateTagRequestTest extends TestCase
         }
     }
 
-    public function testSome()
-    {
-        $auth = (new Authorize)
-            ->setClientId('l76ac1844c563048e582a791871f51f1f5')
-            ->setClientSecret('f4ae9fc64c694b14af5ef3716a902a1b');
-
-        $request = (new CreateTagRequest)
-            ->setAccessToken($auth->authorize()->access_token)
-            ->setAccountNumber(740561073);
-
-
-        var_dump($request);
-        die();
-    }
+//    public function testSome()
+//    {
+//        $auth = (new Authorize)
+//            ->setClientId('l76ac1844c563048e582a791871f51f1f5')
+//            ->setClientSecret('f4ae9fc64c694b14af5ef3716a902a1b');
+//
+//        $request = (new CreateTagRequest)
+//            ->setAccessToken($auth->authorize()->access_token)
+//            ->setAccountNumber(740561073);
+//
+//
+//        var_dump($request);
+//        die();
+//    }
 }
